3dd4cdfa0972a7d1f54923c909f2a951dbafbe1b
irubenboy <rubenmoreno.saladillo@gmail.com>
2020-10-04 12:35:07 +0200

    Creación del proyecto
    Se ha creado el proyecto donde se llevará acabo el desarrollo de aplicació Greet.

--- app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/greet/MainActivity.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import es.iessaladillo.pedrojoya.greet.databinding.MainActivityBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private MainActivityBinding b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
